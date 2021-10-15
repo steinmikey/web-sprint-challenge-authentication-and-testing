04bbf493b730dd57f4c4586814f667febdc20613
Michael Steiner <michaelvsteiner@gmail.com>
2021-10-15 12:52:07 -0600

    implements restricted middleware

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,8 +1,6 @@
 const { findBy } = require("../users/user-model");
 
 async function checkAvailableUsername(req, res, next) {
-  // 4- On FAILED registration due to the `username` being taken,
-  //   the response body should include a string exactly as follows: "username taken".
   const { username } = req.body;
   const [existingUser] = await findBy({ username });
   if (existingUser) {
@@ -13,9 +11,6 @@ async function checkAvailableUsername(req, res, next) {
 }
 
 async function checkCredentials(req, res, next) {
-  // 3- On FAILED registration due to `username` or `password` missing from the request body,
-  //   the response body should include a string exactly as follows: "username and password required".
-
   const { username, password } = req.body;
   if (!username || !password) {
     next({ status: 422, message: `username and password required` });
